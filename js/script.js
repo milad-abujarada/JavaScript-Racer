4983ded4e2873fcddc999fb791fa96183b7699cd
miladabujarada <milad.abujarada@gmail.com>
2017-12-10 15:55:49 -0700

    made the first tank(yellowtank) moves using only the uppercase or lowercase p key

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ console.log("JS works!!");
 let tank1 = document.getElementById("yellowTank");
 let tank1InitialPosition = parseInt(window.getComputedStyle(tank1, null).getPropertyValue("left"));
 
-document.addEventListener('keypress', function(){
-	if(!(document.getElementById("yellowTank").style.left)) {
-		tank1.style.left = String(tank1InitialPosition * 2) + "px";
-		 
-	} else {
+document.addEventListener('keypress', function(event){
+	if ((event.keyCode === 112) || (event.keyCode === 80)){
 
-		tank1.style.left = String(parseInt(tank1.style.left) + tank1InitialPosition) + "px";
+		if(!(document.getElementById("yellowTank").style.left)) {
+			tank1.style.left = String(tank1InitialPosition * 2) + "px";
+			console.log(event.keyCode);
+		} else {
+			console.log(event.keyCode);
+			tank1.style.left = String(parseInt(tank1.style.left) + tank1InitialPosition) + "px";
+		}
 	}
 });
