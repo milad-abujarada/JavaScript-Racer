c2ac7bc4ade30875b5f443f036b12a23b69151d2
miladabujarada <milad.abujarada@gmail.com>
2017-12-10 17:25:30 -0700

    finished making the functions for making the two tanks move and finished the layout of the finish line

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-console.log("JS works!!");
-let tank1 = document.getElementById("yellowTank");
-let tank1InitialPosition = parseInt(window.getComputedStyle(tank1, null).getPropertyValue("left"));
-
-document.addEventListener('keypress', function(event){
-	if ((event.keyCode === 112) || (event.keyCode === 80)){
-
-		if(!(document.getElementById("yellowTank").style.left)) {
-			tank1.style.left = String(tank1InitialPosition * 2) + "px";
-			console.log(event.keyCode);
-		} else {
-			console.log(event.keyCode);
-			tank1.style.left = String(parseInt(tank1.style.left) + tank1InitialPosition) + "px";
+document.addEventListener('DOMContentLoaded', function(){console.log("JS works!!");
+	let movingDistance = initialPosition("orangeTank");
+	document.addEventListener('keypress', function(){ console.log(event.keyCode);
+		if ((event.keyCode === 108) || (event.keyCode === 76) ||(event.keyCode === 97) || (event.keyCode === 65)){
+			if ((event.keyCode === 97) || (event.keyCode === 65)){
+				newPosition(movingDistance, "greenTank");
+			} else {
+				newPosition(movingDistance, "orangeTank");
+			}
 		}
-	}
+	});
 });
+
+function initialPosition(tank){
+	return parseInt(window.getComputedStyle(document.getElementById(tank), null).getPropertyValue("left"));
+};
+
+function newPosition(movingDistance, tank){
+	let movingTank = document.getElementById(tank);
+	if(movingTank.style.left){
+		movingTank.style.left = String(parseInt(movingTank.style.left) + movingDistance) + "px";
+	} else {
+		movingTank.style.left = String(movingDistance * 2) + "px";
+	}; 
+
+}
