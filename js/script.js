2401b03c21b847e64a5f43ca83ed3589d28b38bf
miladabujarada <milad.abujarada@gmail.com>
2017-12-10 18:28:04 -0700

    finished making the logic for setting up the finish line and finding out and declaring the winner

--- js/script.js
+++ js/script.js
@@ -1,26 +1,52 @@
 document.addEventListener('DOMContentLoaded', function(){console.log("JS works!!");
 	let movingDistance = initialPosition("orangeTank");
+	let finishLine = initialPosition("finish");
+	let winner = "";
 	document.addEventListener('keypress', function(){ console.log(event.keyCode);
-		if ((event.keyCode === 108) || (event.keyCode === 76) ||(event.keyCode === 97) || (event.keyCode === 65)){
+		if (((event.keyCode === 108) || (event.keyCode === 76) ||(event.keyCode === 97) || (event.keyCode === 65))&& !winner){
 			if ((event.keyCode === 97) || (event.keyCode === 65)){
 				newPosition(movingDistance, "greenTank");
 			} else {
 				newPosition(movingDistance, "orangeTank");
 			}
 		}
+		if (winner){
+			if (winner === "greenTank"){
+				alert("Green Tank wins!");
+			} else {
+				alert("Orange Tank wins!")
+			};
+			
+		};
 	});
-});
 
-function initialPosition(tank){
-	return parseInt(window.getComputedStyle(document.getElementById(tank), null).getPropertyValue("left"));
-};
+	function initialPosition(tankOrFinishLine){
+	return parseInt(window.getComputedStyle(document.getElementById(tankOrFinishLine), null).getPropertyValue("left"));
+	};
 
-function newPosition(movingDistance, tank){
+	function newPosition(movingDistance, tank){
 	let movingTank = document.getElementById(tank);
 	if(movingTank.style.left){
+		let currentPosition = parseInt(movingTank.style.left) + movingDistance;
 		movingTank.style.left = String(parseInt(movingTank.style.left) + movingDistance) + "px";
+		if (checkForWin(finishLine, currentPosition)){
+			winner = tank;
+		};
 	} else {
 		movingTank.style.left = String(movingDistance * 2) + "px";
 	}; 
 
-}
+	};
+
+	function checkForWin(finishLine, currentMovingTank){
+	if(currentMovingTank >= finishLine){
+		return true;
+	};
+	};
+});
+
+
+
+
+
+
